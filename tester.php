810bd5281be31b06ce6f05cf4f8c43454223595e
ivalrivall <ivalrival95@gmail.com>
2017-11-10 11:45:45 +0700

    Menambahkan HTML dan CSS
    tot

---
+++ tester.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Belajar HTML</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<h1>Belajar HTML</h1>
+	<h1 class="mercu" >Mercu</h1>
+	<div class="success merah">
+		<div class="hitam">
+			
+		</div>
+		
+	</div>
+	<h1 id="mercubuana">fasilkom</h1>
+	<h2 id="mercu">aslab</h1>
+	<h1 id="fasilkom">umb</h1>
+
+	<a href="#" class="button">Link Button</a>
+
+	<table class="tg">
+  <tr>
+    <th >Company</th>
+    <th >Contanct</th>
+    <th >Country</th>
+  </tr>
+  <tr>
+    <td >Andi</td>
+    <td >Budi</td>
+    <td >Ardi</td>
+  </tr>
+  <tr>
+    <td >Santi</td>
+    <td >Elena</td>
+    <td >James</td>
+  </tr>
+  <tr>
+    <td >Haikal</td>
+    <td >Omdar</td>
+    <td >Reigen</td>
+  </tr>
+  <tr>
+    <td >Kali</td>
+    <td >Persi</td>
+    <td >Roger</td>
+  </tr>
+  <tr>
+    <td >Umberlla</td>
+    <td >Tiko</td>
+    <td >Eletin</td>
+  </tr>
+</table>
+<img src="logo mercu hitam.png" alt="">
+</body>
+</html>
